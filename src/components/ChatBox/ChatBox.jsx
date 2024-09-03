a6ec28d678575454be498fb758905949fcdc164b
SudeeraDilshan <new1818155@gmail.com>
2024-09-03 16:06:28 +0530

    added some features to rightsidebar

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -6,8 +6,10 @@ import { onSnapshot, doc, updateDoc } from 'firebase/firestore'
 import { db } from '../../config/firebase'
 import { arrayUnion, getDoc } from '@firebase/firestore'
 import { toast } from 'react-toastify'
-import { uploadBytes, ref } from 'firebase/storage'
+// import { uploadBytes, ref } from 'firebase/storage'
+import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage"; // Ensure you import these functions
 
+const storage = getStorage();
 
 const ChatBox = () => {
   const { userData, messagesId, chatUser, messages, setMessages } = useContext(AppContext);
@@ -54,15 +56,19 @@ const ChatBox = () => {
 
   const sendImage = async (e) => {
     try {
-      const fileURl = await uploadBytes(e.target.files[0]);
-      if (fileURl && messagesId) {
-        await updateDoc(doc(db, 'messeges', messagesId), {
+      const file = e.target.files[0];
+      const storageRef = ref(storage, `images/${file.name}`); // Create a valid reference
+      const snapshot = await uploadBytes(storageRef, file); // Upload the file
+      const fileURL = await getDownloadURL(snapshot.ref); // Get the download URL
+
+      if (fileURL && messagesId) {
+        await updateDoc(doc(db, 'messages', messagesId), {
           messages: arrayUnion({
             sId: userData.id,
-            image: fileURl,
+            image: fileURL,
             createAt: new Date()
           })
-        })
+        });
 
         const userIDs = [chatUser.rId, userData.id];
 
@@ -84,15 +90,57 @@ const ChatBox = () => {
             })
 
           }
-
-        })
-
+        });
       }
     } catch (error) {
+      console.error(error);
       toast.error(error.message);
-      console.log(error);
     }
-  }
+  };
+
+
+
+  // const sendImage = async (e) => {
+  //   try {
+  //     const fileURl = await uploadBytes(e.target.files[0]);
+  //     if (fileURl && messagesId) {
+  //       await updateDoc(doc(db, 'messeges', messagesId), {
+  //         messages: arrayUnion({
+  //           sId: userData.id,
+  //           image: fileURl,
+  //           createAt: new Date()
+  //         })
+  //       })
+
+  //       const userIDs = [chatUser.rId, userData.id];
+
+  //       userIDs.forEach(async (id) => {
+  //         const userChatRef = doc(db, 'chats', id);
+  //         const userChatsSnapshot = await getDoc(userChatRef);
+
+  //         if (userChatsSnapshot.exists()) {
+  //           const userChatData = userChatsSnapshot.data();
+  //           const chatIndex = userChatData.chatsData.findIndex((c) => c.messegeId === messagesId);
+  //           userChatData.chatsData[chatIndex].lastMessage = "Image";
+  //           userChatData.chatsData[chatIndex].updateAt = Date.now();
+  //           if (userChatData.chatsData[chatIndex].rId === userData.id) {
+  //             userChatData.chatsData[chatIndex].messageSeen = false;
+
+  //           }
+  //           await updateDoc(userChatRef, {
+  //             chatsData: userChatData.chatsData
+  //           })
+
+  //         }
+
+  //       })
+
+  //     }
+  //   } catch (error) {
+  //     toast.error(error.message);
+  //     console.log(error);
+  //   }
+  // }
 
 
   const ConvertTimeStamp = (timestamp) => {
